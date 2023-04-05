56c93b670750d9c0d75f719ee6694fe64067d88a
SKpro234 <kylenguyen2002@gmail.com>
2023-04-05 11:02:30 -0700

    squareattackrange

--- aaaa/Assets/Melee.cs
+++ aaaa/Assets/Melee.cs
@@ -10,7 +10,7 @@ public class Melee : MonoBehaviour
 
     public PlayerMovement rightFacing;
     public LayerMask Enemies;
-    public float attackRange;
+    public Vector2 attackRange;
     private Collider2D[] enemiesToDamage;
     void Start()
     {
@@ -31,13 +31,13 @@ public class Melee : MonoBehaviour
     {
          if (Input.GetButtonDown("Fire1")){
             if(rightFacing.GetComponent<PlayerMovement>().isFacingRight){
-            enemiesToDamage = Physics2D.OverlapCircleAll(new Vector2(transform.position.x + 1,transform.position.y),
-            attackRange, Enemies);
+            enemiesToDamage = Physics2D.OverlapBoxAll(new Vector2(transform.position.x + 1,transform.position.y),
+            attackRange, 0, Enemies);
             }
             else
             {
-            enemiesToDamage = Physics2D.OverlapCircleAll(new Vector2(transform.position.x - 1,transform.position.y),
-            attackRange, Enemies);
+            enemiesToDamage = Physics2D.OverlapBoxAll(new Vector2(transform.position.x - 1,transform.position.y),
+            attackRange, 0, Enemies);
             }
             for(int i = 0; i < enemiesToDamage.Length; i++){
                 enemiesToDamage[i].GetComponent<EnemyBehavior>().TakeDamage(damage);
@@ -48,8 +48,8 @@ public class Melee : MonoBehaviour
     
     void OnDrawGizmosSelected(){
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(new Vector2(transform.position.x + 1  ,transform.position.y),attackRange);
-        Gizmos.DrawWireSphere(new Vector2(transform.position.x - 1  ,transform.position.y),attackRange);
+        Gizmos.DrawWireCube(new Vector3(transform.position.x + 1, transform.position.y, transform.position.z), attackRange);
+        Gizmos.DrawWireCube(new Vector3(transform.position.x - 1, transform.position.y, transform.position.z), attackRange);
     }
 
 }
